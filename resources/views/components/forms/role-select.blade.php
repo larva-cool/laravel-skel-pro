bdb776a24d71767e068dc31272aaf2a626a9a5a8
xutongle <xutongle@msn.com>
2026-04-20 19:47:44 +0800

    refactor: 移除未使用的 RoleSelect 组件
    移除 app/View/Components/Forms/RoleSelect.php 及其对应的 Blade 视图文件，
    这些组件已不再使用，清理代码库以减少维护负担。

--- resources/views/components/forms/role-select.blade.php
+++
@@ -1,8 +0,0 @@
-<select name="{{ $name }}" id="{{ $filter }}-select" lay-filter="{{ $filter }}-filter">
-    @if($placeholder)
-    <option value="">{{$placeholder}}</option>
-    @endif
-    @foreach($items as $item)
-        <option value="{{$item['value']}}" @selected($item['value'] == $selected)>{{$item['name']}}</option>
-    @endforeach
-</select>
