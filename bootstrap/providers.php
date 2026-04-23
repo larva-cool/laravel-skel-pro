673dea8501791971c3ccfccda782350f45fd2e29
xutongle <xutongle@msn.com>
2026-04-23 18:14:28 +0800

    feat: 添加 Laravel Horizon 和 Scout 支持
    添加 Laravel Horizon 用于队列监控和管理，并集成 Laravel Scout 提供搜索功能。同时添加了必要的服务提供者、配置文件以及相关依赖包（maatwebsite/excel）。调整了服务提供者的加载顺序。

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DynamicConfigServiceProvider::class,
-    App\Providers\UploadServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\SmsServiceProvider::class,
+    App\Providers\UploadServiceProvider::class,
 ];
