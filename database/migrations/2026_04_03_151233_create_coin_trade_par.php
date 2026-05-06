541e065c6b39d2e1a67d9e81de8a8d703d4d1c41
xutongle <xutongle@msn.com>
2026-05-06 18:46:52 +0800

    fix: 修正数据库分区迁移中的月份计算逻辑
    修复分区名称和分区条件中的月份计算错误，使用动态的月份变量替代硬编码的分区值，确保分区按预期创建

--- database/migrations/2026_04_03_151233_create_coin_trade_par.php
+++ database/migrations/2026_04_03_151233_create_coin_trade_par.php
@@ -7,6 +7,7 @@
 declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
@@ -16,13 +17,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $lastMonth = Carbon::now()->subMonth()->format('pYm');
+        $month = Carbon::now()->format('Y-m-d');
         if (DB::connection()->getConfig('driver') == 'mysql') {
             $tableName = DB::connection()->getTablePrefix().'coin_trades';
             DB::statement("ALTER TABLE `$tableName` DROP PRIMARY KEY, ADD PRIMARY KEY (id, user_id, created_at)");
             DB::statement("ALTER TABLE `$tableName`
             PARTITION BY RANGE ( UNIX_TIMESTAMP(created_at) ) (
-                PARTITION p202603 VALUES LESS THAN (UNIX_TIMESTAMP('2026-04-01')),
-                PARTITION p202604 VALUES LESS THAN (UNIX_TIMESTAMP('2026-05-01')),
+                PARTITION $lastMonth VALUES LESS THAN (UNIX_TIMESTAMP('$month')),
                 PARTITION p_future VALUES LESS THAN MAXVALUE
             )");
         }
