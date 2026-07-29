6f8144bce1e030dbad2a2f5878a6379824151b90
xutongle <xutongle@msn.com>
2026-07-29 10:45:11 +0800

    chore: 更新用户模型和项目规则文档
    1. 为User模型添加Laravel Sanctum的HasApiTokens trait以支持API认证
    2. 整理项目规则文档，修正章节编号和部分内容：
       - 移除前端开发章节
       - 将"API端点"改为"API接口"
       - 移除部署指南中的Jenkins CI/CD管道说明
       - 修正开发设置中的环境文件复制步骤

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * 用户模型
@@ -61,7 +62,7 @@ class User extends Authenticatable
     use DateTimeFormatter;
 
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The model's attributes.
